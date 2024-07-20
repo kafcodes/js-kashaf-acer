2b004efc451fd313ef9dcb803149fe79721bc18e
kafcodes <kashaffatimaiqbal.kfi@gmail.com>
2024-07-20 16:29:21 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS codes as newer version
+
+// alert(3 + 3) // we are using node js, not browser, 
+
+//prioritize code readibility- should be readable and easy to follow//
+
+
+
+let name = "Kashaf"
+let age = 20
+let isLoggedIn = false
+
+
+// number => 2*53
+// bigint 
+// string => prefer ""
+// boolean => true/false (e.g. if the user is logged in/or not)
+// null => standalone value
+// undefined => (that the value has not been defined yet)
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined 
