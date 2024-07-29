16559a45c19d7d82dd1f3d461b27378c369a98c2
kafcodes <kashaffatimaiqbal.kfi@gmail.com>
2024-07-29 17:48:47 +0000

    conversionOperations part 2

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -21,7 +21,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 // "" => false
 // "kaf" => true 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//let value = 3
+//let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " kashaf"
+let str3 = str1 + str2
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");//
+// if string first, then everything becomes string, 
+// if numbers first, then it treats those not in inverted commas as  numbers //
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // don't write like this //
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
